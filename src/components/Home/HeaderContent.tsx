20b3a635acc28ee887ed4d5d5917f36a8ebb7162
Roshan Varghese <roshan.varghese@yahoo.in>
2024-03-28 10:10:25 -0700

    Seperating within year and between year

--- src/components/Home/HeaderContent.tsx
+++ src/components/Home/HeaderContent.tsx
@@ -5,23 +5,28 @@ import Logo from '../../images/HomePage/LogoSVG.svg';
 
 export function HeaderContent(): JSX.Element {
   return (
-    <div className="HeaderContent">
-      <div className="contentWrapper">
-        <div className="logoContainer">
-          <img src={Logo} alt="Logo" className="logo"/>
-        </div>
-        <div className="textContainer">
-          <span className="title">Time Use, Travel, and Telework Dashboard</span>
-          <span className="subtitle"><i>-- by TOMNET and TBD University Transportation Centers</i></span>
+    <div className="mainContainer">
+      <div className="leftColumn"></div> {/* Left section for future content or spacing */}
+      <div className="centerColumn">
+        <div className="HeaderContent">
+          <div className="contentWrapper">
+            <div className="logoContainer">
+              <img src={Logo} alt="Logo" className="logo"/>
+            </div>
+            <div className="textContainer">
+              <span className="title">Time Use, Travel, and Telework Dashboard</span>
+              <span className="subtitle"><i>-- by TOMNET and TBD University Transportation Centers</i></span>
+            </div>
+          </div>
+          <div className="demoButtonContainer">
+            <button className="demoButton">WATCH DEMO</button>
+          </div>
         </div>
       </div>
-      <div className="demoButtonContainer">
-        <button className="demoButton">
-            WATCH DEMO
-        </button>    
-      </div>
-      <div className="graphImageContainer">
-        <img src={graphImage} alt="Dashboard" className="graphImage"/>
+      <div className="rightColumn">
+        <div className="graphImageContainer">
+          <img src={graphImage} alt="Dashboard" className="graphImage"/>
+        </div>
       </div>
     </div>
   );
